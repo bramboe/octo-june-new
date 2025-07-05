1221ff7e1e286375906b5cd1b412570d42e7a5c3
bramboe <37293908+bramboe@users.noreply.github.com>
2025-07-05 18:45:06 +0200

    Fix build: remove all unused code from ESPHome/options.ts for clean RPI/HA add-on build

--- src/ESPHome/options.ts
+++ src/ESPHome/options.ts
@@ -1,17 +1,1 @@
-import { getRootOptions } from '@utils/options';
-
-export interface BLEProxy {
-  host: string;
-  port: number | undefined;
-  password: string | undefined;
-  encryptionKey: string | undefined;
-  expectedServerName: string | undefined;
-}
-
-interface OptionsJson {
-  bleProxies: BLEProxy[];
-}
-
-const options: OptionsJson = getRootOptions();
-
 export const getProxies = () => [];
